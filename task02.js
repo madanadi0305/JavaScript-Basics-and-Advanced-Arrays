4dd955961f28572dfca3d2e620e2543ad5ecd9e0
madanadi0305 <madanadi0305@gmail.com>
2020-09-11 04:56:30 +0000

    Update task02.js

--- task02.js
+++ task02.js
@@ -1,7 +1,8 @@
 function lastNElements(arr,number){
 var n=number;
 var len_arr=arr.length;
-var resultant_array=arr.slice(n,len_arr-1);
+var n1=len_arr-n;
+var resultant_array=arr.slice(n1,len_arr);
 return resultant_array;
 }
 module.exports=lastNElements;
