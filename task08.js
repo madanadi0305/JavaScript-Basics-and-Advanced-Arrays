9bf7ba6fd72d5802907a123214b6e7f23835a787
madanadi0305 <madanadi0305@gmail.com>
2020-09-11 16:22:06 +0000

    Update wiki/task08.md, task08.js files

---
+++ task08.js
@@ -0,0 +1,15 @@
+function sumAndProduct(array){
+  var sum=0;
+  var product=1;
+  var arr1=array;
+  var sum_and_product_array=[];
+  arr1.forEach(function(element){
+      sum=sum+element;
+      product=product*element;
+  });
+   sum_and_product_array.push(sum);
+   sum_and_product_array.push(product);
+    return sum_and_product_array;
+}
+
+module.exports=sumAndProducts;
