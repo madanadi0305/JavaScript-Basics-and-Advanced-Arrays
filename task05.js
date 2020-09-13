0e9e0431bc46fdd534777cc42f8891689bcda554
madanadi0305 <madanadi0305@gmail.com>
2020-09-13 09:28:12 +0000

    Update task05.js

--- task05.js
+++ task05.js
@@ -1,21 +1,24 @@
-function sortInAscendingOrder(array){
-var min,temp;
-for(var i=0;i<array.length;i++){
-    min=i;
-    for(var j=i+1;j<array.length;j++){
-      if(array[j]<array[min]){
-          min=j;
-      }
-
-    }
-
-    if(min!=i){
-    temp=array[min];
-    array[min]=array[j];
-    array[j]=temp;
-    }
+function sortElementsInAscending(array){
+   // var arr1=array;
+    var temp,min;
     
+    for(var i=0;i<array.length;i++){
+        min=i;
+        for(var j=i+1;j<array.length;j++){
+            if(array[j]<array[min]){
+                min=j;
+            }
+            
+            
+        }
+        if(min!=i){
+            temp=array[min];
+            array[min]=array[i];
+            array[i]=temp;
+            
+        }
+        
+    }
+    return array;
 }
-return array;
-}
-module.exports=sortInAscendingOrder;
+module.exports=sortElementsInAscending;
