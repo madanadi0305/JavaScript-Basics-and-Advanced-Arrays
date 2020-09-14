92585a3965d850eeb8724096fe9e8c54e0d176d6
madanadi0305 <madanadi0305@gmail.com>
2020-09-14 12:15:05 +0000

    Update task10.js

--- task10.js
+++ task10.js
@@ -6,7 +6,7 @@ for(var i=0;i<array.length;i++){
 k=array[i];
   for(var j=i+1;j<array.length;j++){
   if(array[j]===k){
-    position=array.indexOf(array[j],1);
+    position=array.indexOf(array[j]);
     next=position+1;
     while(next<array.length)
     {if(array[position]===array[next]){
