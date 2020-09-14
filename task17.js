e8e6ba4f2696e1407bf5788a5f38943bc33741a0
madanadi0305 <madanadi0305@gmail.com>
2020-09-14 06:59:08 +0000

    Update task17.js

--- task17.js
+++ task17.js
@@ -1,20 +1,14 @@
 function findAndRemove(array,number){
-    var pos;
-    var is_Found;
+    //var pos;
+    //var is_Found;
     array.forEach(function(element,index){
-                is_Found=false;
-                  if(number===element){
-                  is_Found=true;
-                  pos=index;
-      
-                 //array.splice(pos,1);
+                
+      if(number===element){
+                //  is_Found=true;
+        array.splice(index,1);
     }
-      if(is_Found===true){
-        array.splice(pos,1);
-       // return array;
-      }
-        
-                  });  
+      
+    });  
     
     
    return array;
