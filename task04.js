1996b09ab31864d2397c2b7d2a9195e078ea2d47
madanadi0305 <madanadi0305@gmail.com>
2020-09-14 10:51:20 +0000

    Update task04.js

--- task04.js
+++ task04.js
@@ -1,8 +1,9 @@
 function repeatedElements(array){
 var array_repeated=[];
-var count,k,is_Max;var maxCount=1;
+var count,k,is_Max;var maxCount=2;
 for(var j=0;j<array.length;j++){
  
+  
  k=array[j];
  for(var i=j;i<array.length;i++){
      if(k===array[i]){
@@ -10,21 +11,24 @@ for(var j=0;j<array.length;j++){
       
       
      }
-    
+  
    if((count>maxCount)){
-   maxCount=count;
+    //is_Max=true;     
+     maxCount=count;
+     //;
+     
    array_repeated.push(k);
-   array_repeated.push(count);
+     array_repeated.push(count);  
          }   
    
  }
- count=0;
-  
-  
+ count=0;  
+ 
   
 }
  
  
+  
  
   
 return array_repeated;
