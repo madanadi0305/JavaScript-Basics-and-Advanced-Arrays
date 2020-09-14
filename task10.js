bed92efb5f5a57179dcb53fcf3634c278901c212
madanadi0305 <madanadi0305@gmail.com>
2020-09-14 12:02:33 +0000

    Update task10.js

--- task10.js
+++ task10.js
@@ -1,17 +1,28 @@
 
 function removeDuplicateElements(array){
+
 var k,position;
 for(var i=0;i<array.length;i++){
 k=array[i];
   for(var j=i+1;j<array.length;j++){
   if(array[j]===k){
-    position=array.indexOf(array[j]);
-    array.splice(position,1);
+    position=array.indexOf(array[j],1);
+    next=position+1;
+    while(next<array.length)
+    {if(array[position]===array[next]){
+      array.splice(next,1);
+      
+    }
+     next=next+1;
+    }
+    
   }
   
   
   }
 }
-return array;
+  return array;
 }
+
+
 module.exports=removeDuplicateElements;
