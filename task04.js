333ac97c61f51753988e6bfd673ebcf6049c6551
madanadi0305 <madanadi0305@gmail.com>
2020-09-14 08:17:44 +0000

    Update task04.js

--- task04.js
+++ task04.js
@@ -1,25 +1,37 @@
 function repeatedElements(array){
-var k,count;
-//var max_count=2;
-var repeated_array=[];
- var is_MaxRepeated;
-for(var i=0;i<array.length;i++){
-    k=array[i];
-    count=1;
-    for(var j=i+1;j<array.length;j++){
-        if(k===array[j]){
-            count=count+1;
-            
-        }
-        if(count>=2){
-            is_MaxRepeated=true;
-        }
-    }
-    if(is_MaxRepeated===true){
-        repeated_array.push(k);
-        repeated_array.push(count);
-    }
+var array_repeated=[];
+var count,k,is_Max;var maxCount=2;
+for(var j=0;j<array.length;j++){
+ count=1;
+  is_Max=false;
+ k=array[j];
+ for(var i=0;i<array.length;i++){
+     if(k===array[i]){
+         count=count+1;
+      
+      
+     }
+  
+   if((count>maxCount)){
+    is_Max=true;       
+    break; 
+         }   
+   
+ }
+  if(is_Max===true){
+  break;
+    //  array_repeated.push(k);
+  //array_repeated.push(count);
+ }
+  
+  
 }
-    return repeated_array;
+ 
+  array_repeated.push(k);
+  array_repeated.push(count)
+ 
+  
+return array_repeated;
 }
+
 module.exports=repeatedElements;
