232aac56e298fa0cb212c6115f284422b99c8368
madanadi0305 <madanadi0305@gmail.com>
2020-09-15 07:45:50 +0000

    Update task21.js

---
+++ task21.js
@@ -0,0 +1,45 @@
+function MaxAndMin(array){
+    var temp,min;
+    var sorted_array;
+    var max_min_array=[];
+    for(var i=0;i<array.length;i++){
+        min=i;
+        for(var j=i+1;j<array.length;j++){
+            if(array[min]>array[j]){
+            min=j;    
+                
+            }
+            
+            
+        }
+        
+        if(min!=i){
+            temp=array[min];
+            array[min]=array[i];
+            array[i]=temp;
+        }
+    }
+    
+sorted_array=array;
+var small=0;
+
+var large=sorted_array.length-1;
+for(var i=0;i<sorted_array.length;i++ ){
+    if(i%2==0){
+       
+       max_min_array[i]=sorted_array[small];
+      small=small+1;;
+        
+    }
+    else{
+        max_min_array[i]=sorted_array[large];
+        large=large-1;
+       
+        
+    }
+    
+    
+}
+    
+return max_min_array;    
+}
