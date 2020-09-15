19ad22a54d509fe2ae28e1d39a5fd379eef6cc9a
madanadi0305 <madanadi0305@gmail.com>
2020-09-15 10:47:03 +0000

    Update task04.js

--- task04.js
+++ task04.js
@@ -1,37 +1,58 @@
-function repeatedElements(array){
-var array_repeated=[];
-var count,k,is_Max;var maxCount=1;
-for(var j=0;j<array.length;j++){
- 
+function mostRepeatedElements(array){
+    var most_repeated=[],newCount;
+    var count,maxcount=1,is_Max,k,max;
+    for(var i=0;i<array.length;i++){
+        count=0;
+        //is_Max=false;
+        for(var j=i;j<array.length;j++)
+        { k=array[j];
+            if(k===array[i]){
+            count=count+1;
+            
+            
+        }
+       // if(count>maxcount ){
+         //   is_Max=true;
+           //  most_repeated.push(k);
+            // most_repeated.push(count);
+            //break;
+       // }
+        if(count>maxcount){
+            //is_Max=true;
+             maxcount=count;
+             //most_repeated.push(k);
+             //most_repeated.push(count);
+            // break;
+        }
+         //maxcount=maxcount+1;   
+        }
+        
+      
+        
+    }
+  max=maxcount;
   
- k=array[j];
- for(var i=j;i<array.length;i++){
-     if(k===array[i]){
-         count=count+1;
+  for(var i=0;i<array.length;i++){
+    newCount=0;
+    for(var j=i;j<array.length;j++){
+      if(array[j]===array[i]){
+        newCount=newCount+1;
+        
+      }
+      if(newCount===max){
+        most_repeated.push(array[j]);
+        most_repeated.push(newCount);
+       // break;
+      }
       
       
-     }
-  
-   if((count>maxCount)){
-    //is_Max=true;     
-     maxCount=count;
-     //;
-     
-   array_repeated.push(k);
-     array_repeated.push(count);  
-         }   
-   
- }
- count=0;  
- 
-  
+    }
+  }
+    
+  //return max;
+   return most_repeated; 
+    
 }
- 
- 
-  
- 
-  
-return array_repeated;
-}
+module.exports=mostRepeatedElements;
 
-module.exports=repeatedElements;
+
